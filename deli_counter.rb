1ca82a844b1028863246b4b5d1e93f650cb2919f
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-06 13:39:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def line(array)
+
+
+#line and position
+def line(katz_deli)
+  if katz_deli >= 1
+    puts "The line is currently empty."
+  else
+   puts "the line is currently #{line(katz_deli)}"
+  end
+end
+
+#take_a_number
+
+
+#now_serving
