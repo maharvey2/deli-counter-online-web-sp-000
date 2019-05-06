17c02014268eb709f8bf5c095ae17bfa038e349e
Mark Harvey <maharvey2@crimson.ua.edu>
2019-05-06 14:03:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,15 +13,7 @@ def line(array)
     puts "The line is currently empty."
   end
 end
-line(katz_deli)
-#line and position
-def line(katz_deli)
-  if katz_deli >= 1
-    puts "The line is currently empty."
-  else
-   puts "the line is currently #{line(katz_deli)}"
-  end
-end
+
 
 #take_a_number
 def take_a_number(line, new_person)
